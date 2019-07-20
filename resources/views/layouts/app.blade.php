7a9ddea69205147fab6c655fc1d7c2b574472ac8
tanjeeb <tanjeeb21@gmail.com>
2019-07-20 22:34:56 +0600

    make home view

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
+    <!-- Styles -->
+    <link type="text/css" rel="stylesheet" href="{{mix('css/app.css')}}">
+</head>
+<body class="site">
+@include('layout.header')
+<div class="main">
+    @yield('content')
+</div>
+@include('layout.footer')
+
+
+<script type="text/javascript" src="{{mix('js/app.js')}}"></script>
+</body>
+</html>
